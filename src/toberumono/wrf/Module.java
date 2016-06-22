f835aa38b6e10ca890145c3535d8aabc7b4c8833
Toberumono <JoshuaLipstone@gmail.com>
2016-06-22 15:01:44 -0500

    Documentation for Module.

--- src/toberumono/wrf/Module.java
+++ src/toberumono/wrf/Module.java
@@ -27,6 +27,14 @@ public abstract class Module extends AbstractScope<Simulation> {
 	private Namelist namelist;
 	private ScopedList dependencies;
 	
+	/**
+	 * Constructs a new {@link Module} defined by the given {@link ModuleScopedMap parameters} and initialized by the given {@link Simulation}.
+	 * 
+	 * @param parameters
+	 *            the parameters that define the {@link Module}
+	 * @param sim
+	 *            the {@link Simulation} that initialized the {@link Module}
+	 */
 	public Module(ModuleScopedMap parameters, Simulation sim) {
 		super(sim);
 		this.parameters = parameters;
@@ -40,33 +48,87 @@ public abstract class Module extends AbstractScope<Simulation> {
 		module = (ScopedMap) parameters.get("module");
 	}
 	
+	/**
+	 * Parses {@link Timing} information from the given {@link ScopedMap}.
+	 * 
+	 * @param timing
+	 *            the {@link ScopedMap} containing the {@link Module Module's} {@link Timing} information
+	 * @return the parsed {@link Timing} information
+	 */
 	protected Timing parseTiming(ScopedMap timing) {
 		return WRFRunnerComponentFactory.generateComponent(Timing.class, timing, getSim().getTiming());
 	}
 	
+	/**
+	 * Reads the {@link Namelist} data for the {@link Module} from disk.<br>
+	 * <b>Note:</b> The path to the file that the {@link Namelist} data is read from is defined by
+	 * {@code getSim().getSourcePath(getName()).resolve(getNamelistPath())}
+	 * 
+	 * @return the data in the {@link Namelist} file as a {@link Namelist} object
+	 * @throws IOException
+	 *             if an I/O error occurs while reading from the {@link Namelist} file
+	 */
 	protected Namelist ingestNamelist() throws IOException {
 		if (getNamelistPath() != null && getSim().getSourcePath(getName()) != null)
 			return new Namelist(getSim().getSourcePath(getName()).resolve(getNamelistPath()));
 		return null;
 	}
 	
+	/**
+	 * Writes the {@link Module Module's} {@link Namelist} data to disk.<br>
+	 * <b>Note:</b> The path to the file that the {@link Namelist} data is written to is defined by
+	 * {@code getSim().getActivePath(getName()).resolve(getNamelistPath())}
+	 * 
+	 * @throws IOException
+	 *             if an I/O error occurs while writing to the {@link Namelist} file
+	 */
 	protected void writeNamelist() throws IOException {
 		if (getNamelist() != null && getSim().getActivePath(getName()) != null)
 			getNamelist().write(getSim().getActivePath(getName()).resolve(getNamelistPath()));
 	}
 	
+	/**
+	 * The logic to update the {@link Module Module's} {@link Namelist} file
+	 * 
+	 * @throws IOException
+	 *             if an I/O error occurs while updating the {@link Namelist} file
+	 * @throws InterruptedException
+	 *             if the process is interrupted
+	 */
 	public abstract void updateNamelist() throws IOException, InterruptedException;
 	
+	/**
+	 * The logic to execute the subprocesses that form the {@link Module Module's} step in the {@link Simulation}.
+	 * 
+	 * @throws IOException
+	 *             if an I/O error occurs while running the subprocesses
+	 * @throws InterruptedException
+	 *             if the process is interrupted
+	 */
 	public abstract void execute() throws IOException, InterruptedException;
 	
+	/**
+	 * Cleans up the files that are no longer of use to the {@link Simulation}. This is run after {@link #execute()} returned.
+	 * 
+	 * @throws IOException
+	 *             if an I/O error occurs while cleaning up files
+	 * @throws InterruptedException
+	 *             if the process is interrupted
+	 */
 	public abstract void cleanUp() throws IOException, InterruptedException;
 	
+	/**
+	 * @return the {@link Path} to the {@link Module Module's} namelist file relative to the {@link Module Module's} root directory
+	 */
 	public Path getNamelistPath() { //namelistPath holds the location of the namelist file relative to the module's root directory
 		if (namelistPath == null)
 			namelistPath = (module.containsKey(NAMELIST_FIELD_NAME) ? Paths.get((String) module.get(NAMELIST_FIELD_NAME)) : null);
 		return namelistPath;
 	}
 	
+	/**
+	 * @return the {@link Module Module's} processed {@link Timing} information
+	 */
 	@NamedScopeValue("timing")
 	public Timing getTiming() {
 		if (timing != null) //First one is to avoid unnecessary use of synchronization
@@ -78,6 +140,9 @@ public abstract class Module extends AbstractScope<Simulation> {
 		}
 	}
 	
+	/**
+	 * @return a {@link ScopedList} wherein each value is a {@link Module} that must be run before this {@link Module} can be run
+	 */
 	public ScopedList getDependencies() {
 		if (dependencies != null) //First one is to avoid unnecessary use of synchronization
 			return dependencies;
@@ -97,14 +162,25 @@ public abstract class Module extends AbstractScope<Simulation> {
 		return dependencies;
 	}
 	
+	/**
+	 * @return the {@link Module Module's} name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * @return the {@link Simulation} that initialized the {@link Module}
+	 */
 	public Simulation getSim() {
 		return getParent();
 	}
 	
+	/**
+	 * @return the {@link Module Module's} {@link Namelist} file
+	 * @throws IOException
+	 *             if an I/O error occurs while loading the {@link Namelist} file
+	 */
 	public Namelist getNamelist() throws IOException {
 		if (namelist != null) //First one is to avoid unnecessary use of synchronization
 			return namelist;
@@ -114,7 +190,10 @@ public abstract class Module extends AbstractScope<Simulation> {
 			return namelist = ingestNamelist();
 		}
 	}
-
+	
+	/**
+	 * @return the parameters that defined the {@link Module} as a {@link ScopedMap}
+	 */
 	@NamedScopeValue("parameters")
 	public ScopedMap getParameters() {
 		return parameters;
