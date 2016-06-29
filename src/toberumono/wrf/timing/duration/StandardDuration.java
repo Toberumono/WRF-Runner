ae61510669f10ee2621dbc6d95a615ed27f1c8db
Toberumono <JoshuaLipstone@gmail.com>
2016-06-29 16:42:57 -0500

    Added inheritance logic to StandardDuration.

--- src/toberumono/wrf/timing/duration/StandardDuration.java
+++ src/toberumono/wrf/timing/duration/StandardDuration.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 import java.util.logging.Logger;
 
 import toberumono.wrf.scope.Scope;
+import toberumono.wrf.scope.ScopedFormulaProcessor;
 import toberumono.wrf.scope.ScopedMap;
 
 import static toberumono.wrf.SimulationConstants.*;
@@ -39,7 +40,15 @@ public class StandardDuration extends AbstractDuration {
 	protected void compute() {
 		duration = new int[TIMING_FIELD_NAMES.size()];
 		for (int i = 0; i < duration.length; i++)
-			if (getParameters().containsKey(TIMING_FIELD_NAMES.get(i))) //TODO implement inheritance via checking for String values equal to "inherit"
-				duration[i] = ((Number) getParameters().get(TIMING_FIELD_NAMES.get(i))).intValue();
+			if (getParameters().containsKey(TIMING_FIELD_NAMES.get(i))) {
+				Object val = getParameters().get(TIMING_FIELD_NAMES.get(i));
+				if (val instanceof String)
+					val = ScopedFormulaProcessor.process((String) val, this, TIMING_FIELD_NAMES.get(i));
+				if (val instanceof Number)
+					duration[i] = ((Number) val).intValue();
+				else
+					throw new IllegalArgumentException(
+							"The value of the " + TIMING_FIELD_NAMES.get(i) + " field must be an instance of Number or evaluate to an instance of Number after inheritance is processed.");
+			}
 	}
 }
