a3e2bc124138436e645da101b9603c699c60d2e1
Toberumono <JoshuaLipstone@gmail.com>
2016-07-02 15:49:58 -0500

    Documentation and better processing for Bucket- and FunctionRound.

--- src/toberumono/wrf/timing/round/BucketRound.java
+++ src/toberumono/wrf/timing/round/BucketRound.java
@@ -5,85 +5,108 @@ import java.util.Calendar;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Function;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import toberumono.utils.general.Numbers;
-import toberumono.wrf.scope.ScopedMap;
 import toberumono.wrf.scope.Scope;
-import toberumono.wrf.scope.ScopedList;
+import toberumono.wrf.scope.ScopedMap;
 
 import static toberumono.wrf.SimulationConstants.*;
 
+/**
+ * Implementation of {@link Round} that places values into buckets to round them.
+ * 
+ * @author Toberumono
+ */
 public class BucketRound extends AbstractRound {
 	private Map<Integer, Function<Integer, Integer>> roundingActions;
 	
+	/**
+	 * Initializes a new instance of {@link BucketRound} described by the given {@code parameters} with a {@link Logger} derived from
+	 * {@link Round#LOGGER_NAME} and the given parent {@link Scope}.
+	 * 
+	 * @param parameters
+	 *            the parameters that describe the implementation as a {@link ScopedMap}
+	 * @param parent
+	 *            the parent {@link Scope}
+	 */
 	public BucketRound(ScopedMap parameters, Scope parent) {
 		super(parameters, parent);
 	}
 	
-	private static final Map<Integer, Function<Integer, Integer>> parseRoundingParameters(ScopedMap parameters) { //TODO implement input scrubbing and error messages
-		Object enabled = parameters.get("enabled");
-		Collection<String> keep = TIMING_FIELD_NAMES;
-		if (enabled instanceof ScopedList)
-			keep = ((ScopedList) enabled).stream().filter(e -> e instanceof String && TIMING_FIELD_NAMES.contains(e)).map(e -> (String) e).collect(Collectors.toSet());
-		else if (enabled instanceof ScopedMap) {
-			keep = ((ScopedMap) enabled).entrySet().stream().filter(e -> TIMING_FIELD_NAMES.contains(e.getKey()) && (!(e.getValue() instanceof Boolean) || (Boolean) e.getValue()))
-					.map(e -> e.getKey()).collect(Collectors.toSet());
+	private final Map<Integer, Function<Integer, Integer>> parseRoundingParameters(Map<?, ?> parameters) { //TODO implement input scrubbing and error messages
+		Object enabled = parameters.get("enabled"); //enabled could be a List or Map
+		Collection<String> keep = TIMING_FIELD_NAMES; //If enabled isn't a List or Map, every timing field will be counted as enabled
+		if (enabled instanceof List)
+			keep = ((List<?>) enabled).stream().filter(e -> e instanceof String && TIMING_FIELD_NAMES.contains(e)).map(e -> (String) e).collect(Collectors.toSet());
+		else if (enabled instanceof Map) {
+			keep = ((Map<?, ?>) enabled).entrySet().stream().filter(e -> TIMING_FIELD_NAMES.contains(e.getKey()) && (!(e.getValue() instanceof Boolean) || (Boolean) e.getValue()))
+					.map(e -> (String) e.getKey()).collect(Collectors.toSet());
 		}
 		else if (enabled instanceof String) {
 			keep = new HashSet<>();
 			keep.add((String) enabled);
 		}
 		Map<Integer, Function<Integer, Integer>> roundingFunctions = new HashMap<>();
-		ScopedMap arguments = (ScopedMap) parameters.get("arguments");
+		Map<?, ?> arguments = (Map<?, ?>) parameters.get("arguments");
 		String name, arg;
-		RoundingMode globalRM = arguments.containsKey("rounding-mode") ? (arguments.get("rounding-mode") instanceof String ? RoundingMode.valueOf(((String) arguments.get("rounding-mode")).toUpperCase())
-				: RoundingMode.valueOf(((Number) arguments.get("rounding-mode")).intValue())) : RoundingMode.FLOOR;
+		Object value = arguments.get("rounding-mode");
+		RoundingMode globalRM =
+				value instanceof String ? RoundingMode.valueOf(((String) value).toUpperCase()) : (value instanceof Number ? RoundingMode.valueOf(((Number) value).intValue()) : RoundingMode.FLOOR);
 		for (int i = 0; i < TIMING_FIELD_NAMES.size(); i++) {
-			if (!keep.contains(name = TIMING_FIELD_NAMES.get(i)))
+			name = TIMING_FIELD_NAMES.get(i);
+			if (!keep.contains(name))
 				continue;
-			Object value = arguments.containsKey(name) ? arguments.get(name) : null;
+			value = arguments.containsKey(name) ? arguments.get(name) : null;
 			arg = name + "-rounding-mode";
 			final RoundingMode rm = arguments.containsKey(arg) ? (arguments.get(arg) instanceof String //TODO try to reduce calls to get here
 					? RoundingMode.valueOf(((String) arguments.get(arg)).toUpperCase()) : RoundingMode.valueOf(((Number) arguments.get(arg)).intValue())) : globalRM;
 			if (value == null) { //Step-offset
-				//TODO implement existence checks and enforce Integer type requirement
-				final int step = ((Number) arguments.get(name + "-step")).intValue();
-				final int offset = arguments.containsKey(name + "-offset") ? ((Number) arguments.get(name + "-offset")).intValue() : 0;
+				String field = name + "-step";
+				Object num = arguments.get(field);
+				final int step = evaluateToNumber(num, field).intValue();
+				
+				field = name + "-offset";
+				num = arguments.get(field);
+				final int offset = evaluateToNumber(num != null ? num : 0, field).intValue();
 				roundingFunctions.put(TIMING_FIELD_IDS.get(i), inp -> Numbers.bucketRounding(inp, rm, step, offset));
 			}
-			else if (value instanceof ScopedList) { //Explicit buckets
-				ScopedList temp = (ScopedList) value;
-				int[] buckets = new int[temp.size()];
+			else if (value instanceof List) { //Explicit buckets
+				List<?> temp = (List<?>) value;
+				final int[] buckets = new int[temp.size()];
 				for (int b = 0; b < buckets.length; b++)
-					buckets[b] = ((Number) temp.get(b)).intValue(); //TODO enforce Integer type requirement
+					buckets[b] = evaluateToNumber(temp.get(b), name + "[" + b + "]").intValue();
 				roundingFunctions.put(TIMING_FIELD_IDS.get(i), inp -> Numbers.bucketRounding(inp, rm, buckets));
 			}
-			else if (value instanceof ScopedMap) {
-				ScopedMap temp = (ScopedMap) value;
+			else if (value instanceof Map) {
+				Map<?, ?> temp = (Map<?, ?>) value;
 				final RoundingMode orm = temp.containsKey("rounding-mode") ? (temp.get("rounding-mode") instanceof String //TODO try to reduce calls to get here
 						? RoundingMode.valueOf(((String) temp.get("rounding-mode")).toUpperCase()) : RoundingMode.valueOf(((Number) temp.get("rounding-mode")).intValue())) : rm;
 				if (temp.containsKey("buckets")) {
-					ScopedList bucks = (ScopedList) temp.get("buckets"); //TODO enforce array type requirement
-					int[] buckets = new int[bucks.size()];
+					List<?> bucks = evaluateToType(temp.get("buckets"), name + ".buckets", List.class);
+					final int[] buckets = new int[bucks.size()];
 					for (int b = 0; b < buckets.length; b++)
-						buckets[b] = ((Number) bucks.get(b)).intValue(); //TODO enforce Integer type requirement
+						buckets[b] = evaluateToNumber(bucks.get(b), name + ".buckets[" + b + "]").intValue();
 					roundingFunctions.put(TIMING_FIELD_IDS.get(i), inp -> Numbers.bucketRounding(inp, rm, buckets));
 				}
 				else {
-					//TODO implement existence checks and enforce Integer type requirement
-					final int step = ((Number) arguments.get("step")).intValue();
-					final int offset = arguments.containsKey("offset") ? ((Number) arguments.get("offset")).intValue() : 0;
+					final int step = evaluateToNumber(arguments.get("step"), name).intValue();
+					final int offset = evaluateToNumber(arguments.containsKey("offset") ? arguments.get("offset") : 0, "offset").intValue();
 					roundingFunctions.put(TIMING_FIELD_IDS.get(i), inp -> Numbers.bucketRounding(inp, orm, step, offset));
 				}
 			}
 			else if (value instanceof Number) {
 				final int step = ((Number) value).intValue();
-				final int offset = arguments.containsKey(name + "-offset") ? ((Number) arguments.get(name + "-offset")).intValue() : 0;
+				final int offset = evaluateToNumber(arguments.containsKey(name + "-offset") ? arguments.get(name + "-offset") : 0, name + "-offset").intValue();
 				roundingFunctions.put(TIMING_FIELD_IDS.get(i), inp -> Numbers.bucketRounding(inp, rm, step, offset));
+			}
+			else {
+				throw new IllegalArgumentException("The value of " + name + " in BucketRound must be either undefined, null, or an instance of List, Map, or Number.");
 			}
 		}
 		return roundingFunctions;
@@ -91,14 +114,16 @@ public class BucketRound extends AbstractRound {
 	
 	@Override
 	protected Calendar doApply(Calendar base) {
-		Calendar out = (Calendar) base.clone();
 		for (Entry<Integer, Function<Integer, Integer>> e : roundingActions.entrySet())
-			out.set(e.getKey(), e.getValue().apply(out.get(e.getKey())));
-		return out;
+			base.set(e.getKey(), e.getValue().apply(base.get(e.getKey())));
+		return base;
 	}
 	
 	@Override
-	protected void compute() { //TODO implement inheritance
-		roundingActions = parseRoundingParameters(getParameters());
+	protected void compute() {
+		if (getParameters() == null)
+			roundingActions = getParent() instanceof BucketRound ? parseRoundingParameters(((BucketRound) getParent()).getParameters()) : new HashMap<>();
+		else
+			roundingActions = parseRoundingParameters(getParameters());
 	}
 }
--- src/toberumono/wrf/timing/round/FunctionRound.java
+++ src/toberumono/wrf/timing/round/FunctionRound.java
@@ -1,6 +1,7 @@
 package toberumono.wrf.timing.round;
 
 import java.util.Calendar;
+import java.util.logging.Logger;
 
 import toberumono.structures.sexpressions.ConsCell;
 import toberumono.wrf.scope.Scope;
@@ -9,9 +10,24 @@ import toberumono.wrf.scope.ScopedMap;
 import static toberumono.wrf.SimulationConstants.*;
 import static toberumono.wrf.scope.ScopedFormulaProcessor.*;
 
+/**
+ * An implementation of {@link Round} that allows the user to define functions that convert the unrounded timing values to their final "rounded"
+ * values.
+ * 
+ * @author Toberumono
+ */
 public class FunctionRound extends AbstractRound {
 	private ConsCell[] functions;
 	
+	/**
+	 * Initializes a new instance of {@link FunctionRound} described by the given {@code parameters} with a {@link Logger} derived from
+	 * {@link Round#LOGGER_NAME} and the given parent {@link Scope}.
+	 * 
+	 * @param parameters
+	 *            the parameters that describe the implementation as a {@link ScopedMap}
+	 * @param parent
+	 *            the parent {@link Scope}
+	 */
 	public FunctionRound(ScopedMap parameters, Scope parent) {
 		super(parameters, parent);
 		functions = null;
@@ -23,7 +39,7 @@ public class FunctionRound extends AbstractRound {
 		for (int i = 0; i < functions.length; i++)
 			manufacturedScope.put(TIMING_FIELD_NAMES.get(i), base.get(TIMING_FIELD_IDS.get(i)));
 		for (int i = 0; i < functions.length; i++)
-			base.set(TIMING_FIELD_IDS.get(i), ((Number) process(functions[i], manufacturedScope, TIMING_FIELD_NAMES.get(i)).getCar()).intValue());
+			base.set(TIMING_FIELD_IDS.get(i), evaluateToNumber(process(functions[i], manufacturedScope, TIMING_FIELD_NAMES.get(i)).getCar(), TIMING_FIELD_NAMES.get(i)).intValue());
 		return base;
 	}
 	
