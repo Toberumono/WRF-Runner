8c3db720544332364b219a51b23b6bdb5bb5afaa
Toberumono <JoshuaLipstone@gmail.com>
2016-07-09 15:36:23 -0500

    Reworked BucketRound's parse function for cleanliness
    * Uses parseEnabled from AbstractTimingComponent
    * Spun logic for creating the rounding functions off into methods
    * Switched over to better input scrubbing and error detection messages
    * Made using an "arguments" subsection optional (It is only kept in at all for backwards-compatibility.

--- src/toberumono/wrf/timing/round/BucketRound.java
+++ src/toberumono/wrf/timing/round/BucketRound.java
@@ -4,13 +4,11 @@ import java.math.RoundingMode;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 import toberumono.utils.general.Numbers;
 import toberumono.wrf.scope.Scope;
@@ -39,77 +37,72 @@ public class BucketRound extends AbstractRound {
 		super(parameters, parent);
 	}
 	
-	private final Map<Integer, Function<Integer, Integer>> parseRoundingParameters(Map<?, ?> parameters) { //TODO implement input scrubbing and error messages
-		Object enabled = parameters.get("enabled"); //enabled could be a List or Map
-		Collection<String> keep = TIMING_FIELD_NAMES; //If enabled isn't a List or Map, every timing field will be counted as enabled
-		if (enabled instanceof List)
-			keep = ((List<?>) enabled).stream().filter(e -> e instanceof String && TIMING_FIELD_NAMES.contains(e)).map(e -> (String) e).collect(Collectors.toSet());
-		else if (enabled instanceof Map) {
-			keep = ((Map<?, ?>) enabled).entrySet().stream().filter(e -> TIMING_FIELD_NAMES.contains(e.getKey()) && (!(e.getValue() instanceof Boolean) || (Boolean) e.getValue()))
-					.map(e -> (String) e.getKey()).collect(Collectors.toSet());
-		}
-		else if (enabled instanceof String) {
-			keep = new HashSet<>();
-			keep.add((String) enabled);
-		}
-		Map<Integer, Function<Integer, Integer>> roundingFunctions = new HashMap<>();
-		Map<?, ?> arguments = (Map<?, ?>) parameters.get("arguments");
-		String name, arg;
-		Object value = arguments.get("rounding-mode");
-		RoundingMode globalRM =
-				value instanceof String ? RoundingMode.valueOf(((String) value).toUpperCase()) : (value instanceof Number ? RoundingMode.valueOf(((Number) value).intValue()) : RoundingMode.FLOOR);
+	private final Map<Integer, Function<Integer, Integer>> parseRoundingParameters(Map<?, ?> parameters) {
+		Collection<String> keep = parseEnabled();
+		Map<Integer, Function<Integer, Integer>> roundingFunctions = new HashMap<>(); //We use a map because the key is the TIMING_FIELD_ID
+		Map<?, ?> arguments = parameters.containsKey("arguments") ? (Map<?, ?>) parameters.get("arguments") : parameters; //Having the arguments sub-object was pointless
+		String name;
+		Object value;
+		RoundingMode globalRM = roundingModeFromField(arguments.get("rounding-mode"), RoundingMode.FLOOR);
 		for (int i = 0; i < TIMING_FIELD_NAMES.size(); i++) {
 			name = TIMING_FIELD_NAMES.get(i);
 			if (!keep.contains(name))
 				continue;
-			value = arguments.containsKey(name) ? arguments.get(name) : null;
-			arg = name + "-rounding-mode";
-			final RoundingMode rm = arguments.containsKey(arg) ? (arguments.get(arg) instanceof String //TODO try to reduce calls to get here
-					? RoundingMode.valueOf(((String) arguments.get(arg)).toUpperCase()) : RoundingMode.valueOf(((Number) arguments.get(arg)).intValue())) : globalRM;
+			value = arguments.get(name); //If arguments does not contain name, arguments.get(name) returns null
+			final RoundingMode rm = roundingModeFromField(arguments.get(name + "-rounding-mode"), globalRM);
 			if (value == null) { //Step-offset
 				String field = name + "-step";
-				Object num = arguments.get(field);
-				final int step = evaluateToNumber(num, field).intValue();
+				final int step = evaluateToNumber(arguments.get(field), field).intValue();
 				
 				field = name + "-offset";
-				num = arguments.get(field);
-				final int offset = evaluateToNumber(num != null ? num : 0, field).intValue();
-				roundingFunctions.put(TIMING_FIELD_IDS.get(i), inp -> Numbers.bucketRounding(inp, rm, step, offset));
+				final int offset = evaluateToNumber(arguments.containsKey(field) ? arguments.get(field) : 0, field).intValue();
+				roundingFunctions.put(TIMING_FIELD_IDS.get(i), stepOffsetProcessor(rm, step, offset));
 			}
 			else if (value instanceof List) { //Explicit buckets
-				List<?> temp = (List<?>) value;
-				final int[] buckets = new int[temp.size()];
-				for (int b = 0; b < buckets.length; b++)
-					buckets[b] = evaluateToNumber(temp.get(b), name + "[" + b + "]").intValue();
-				roundingFunctions.put(TIMING_FIELD_IDS.get(i), inp -> Numbers.bucketRounding(inp, rm, buckets));
+				roundingFunctions.put(TIMING_FIELD_IDS.get(i), explicitBucketsProcessor(rm, name, (List<?>) value));
 			}
 			else if (value instanceof Map) {
 				Map<?, ?> temp = (Map<?, ?>) value;
-				final RoundingMode orm = temp.containsKey("rounding-mode") ? (temp.get("rounding-mode") instanceof String //TODO try to reduce calls to get here
-						? RoundingMode.valueOf(((String) temp.get("rounding-mode")).toUpperCase()) : RoundingMode.valueOf(((Number) temp.get("rounding-mode")).intValue())) : rm;
-				if (temp.containsKey("buckets")) {
-					List<?> bucks = evaluateToType(temp.get("buckets"), name + ".buckets", List.class);
-					final int[] buckets = new int[bucks.size()];
-					for (int b = 0; b < buckets.length; b++)
-						buckets[b] = evaluateToNumber(bucks.get(b), name + ".buckets[" + b + "]").intValue();
-					roundingFunctions.put(TIMING_FIELD_IDS.get(i), inp -> Numbers.bucketRounding(inp, rm, buckets));
-				}
-				else {
-					final int step = evaluateToNumber(arguments.get("step"), name).intValue();
-					final int offset = evaluateToNumber(arguments.containsKey("offset") ? arguments.get("offset") : 0, "offset").intValue();
-					roundingFunctions.put(TIMING_FIELD_IDS.get(i), inp -> Numbers.bucketRounding(inp, orm, step, offset));
-				}
+				final RoundingMode orm = roundingModeFromField(temp.get("rounding-mode"), rm);
+				if (temp.containsKey("buckets"))
+					roundingFunctions.put(TIMING_FIELD_IDS.get(i), explicitBucketsProcessor(orm, name + ".buckets", evaluateToType(temp.get("buckets"), name + ".buckets", List.class)));
+				else
+					roundingFunctions.put(TIMING_FIELD_IDS.get(i), stepOffsetProcessor(orm, evaluateToNumber(temp.get("step"), name + ".step").intValue(),
+							evaluateToNumber(temp.containsKey("offset") ? arguments.get("offset") : 0, name + ".offset").intValue()));
 			}
-			else if (value instanceof Number) {
-				final int step = ((Number) value).intValue();
-				final int offset = evaluateToNumber(arguments.containsKey(name + "-offset") ? arguments.get(name + "-offset") : 0, name + "-offset").intValue();
-				roundingFunctions.put(TIMING_FIELD_IDS.get(i), inp -> Numbers.bucketRounding(inp, rm, step, offset));
+			else if (value instanceof Number) { //Step-offset without the -step tag
+				String field = name + "-offset";
+				roundingFunctions.put(TIMING_FIELD_IDS.get(i),
+						stepOffsetProcessor(rm, ((Number) value).intValue(), evaluateToNumber(arguments.containsKey(field) ? arguments.get(field) : 0, field).intValue()));
 			}
 			else {
 				throw new IllegalArgumentException("The value of " + name + " in BucketRound must be either undefined, null, or an instance of List, Map, or Number.");
 			}
 		}
 		return roundingFunctions;
+	}
+	
+	private Function<Integer, Integer> stepOffsetProcessor(RoundingMode rm, int step, int offset) {
+		return inp -> Numbers.bucketRounding(inp, rm, step, offset);
+	}
+	
+	private Function<Integer, Integer> explicitBucketsProcessor(RoundingMode rm, String name, List<?> buckets) {
+		final int[] bucketsArr = new int[buckets.size()];
+		for (int b = 0; b < bucketsArr.length; b++)
+			bucketsArr[b] = evaluateToNumber(buckets.get(b), name + "[" + b + "]").intValue();
+		return inp -> Numbers.bucketRounding(inp, rm, bucketsArr);
+	}
+	
+	private RoundingMode roundingModeFromField(Object value, RoundingMode defaultRM) {
+		if (value == null)
+			return defaultRM;
+		if (value instanceof String) {
+			try {
+				return RoundingMode.valueOf(((String) value).toUpperCase());
+			}
+			catch (IllegalArgumentException e) {/*Nothing to do here - we're just trying to get the Enum value from the String.*/}
+		}
+		return RoundingMode.valueOf(evaluateToNumber(value, "rounding-mode").intValue());
 	}
 	
 	@Override
